ce040bc83aec7b56d8a2f16e847ee714d9df8add
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-06-11 13:35:07 +0300

    Добавлен panic recover

--- cmd/http_server/main.go
+++ cmd/http_server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
+	"runtime/debug"
 	"syscall"
 
 	"github.com/passwordhash/task-manager-api/internal/app"
@@ -11,6 +13,13 @@ import (
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(os.Stderr, "application panic: %v\n\n%s", r, debug.Stack())
+			os.Exit(1)
+		}
+	}()
+
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
 
